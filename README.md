e00c7f2040350a8a157255cb4d05ccb6c2dbc507
manikanta2901 <manikanta2901@gmail.com>
2021-07-08 14:12:28 +0530

    After working widgets

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## This directory contains implementations of Various Widgets present  in Android
+
+> Contains implementations of
+> 1.Linear Layout -> MainActivity
+> > * Toaster
+> > * Button
+> > * EditTextView
+> > * Radion Button 
--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.android_basics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    private var button : Button ?= null
+    private var name : TextView ?= null
+    private var email : EditText ?=null
+    private var r1 : RadioButton ?= null
+    private var r2 : RadioButton ?= null
+//    private var rb : RadioGroup ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById<Button>(R.id.button)
+        name = findViewById<TextView>(R.id.textView3)
+        r1 = findViewById<RadioButton>(R.id.radioButton6)
+        r2 = findViewById<RadioButton>(R.id.radioButton7)
+//        rb = findViewById<RadioGroup>(R.id.radioGroup)
+        email = findViewById<EditText>(R.id.email)
+
+        button?.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+//                Toast.makeText(applicationContext, "Button is Clicked!!!", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@MainActivity, "Button is Clicked!!!", Toast.LENGTH_LONG).show()
+                println("Name Manikanta\n email ${email?.text}")
+            }
+        })
     }
 }
